0307ee535d314c0d0d566f00a37fe9827f0971ed
Gliperv7 <klimenkoarik4@gmail.com>
2025-10-20 14:56:47 +0300

    Header done, work on sections started

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+let burgerBtn = document.querySelector('.burger-btn');
+let burgerMenu = document.querySelector('.burger-menu');
+burgerBtn.addEventListener('click', () => {
+    burgerMenu.classList.toggle('show');
+})
